3ce2aedff19da79efb8059feadf9b4c644ce56dc
bobsamss <boby-samuel@thebodyshop.co.id>
2024-10-29 14:34:09 +0700

    tambah voucher redemption my voucher

---
+++ cypress/e2e/Online Voucher Redemption/Voucher-Redemption-Myvoucher.cy.js
@@ -0,0 +1,25 @@
+describe('General API Test Group', () => {
+  it('Shows My voucher yg sudah direedem', () => {})
+  it('Shows My voucher yg sudah dipake', () => {})
+  it('Shows My voucher yg sudah expired, harusnya masuk ke tab Pas Voucher', () => {})
+
+  // ---- technical sections
+  // wrong ID format: non-MongoDB, undefined, empty
+  it('', () => {})
+
+  // not found ID
+  it('', () => {})
+
+  // ---- business sections
+  // what data to show?
+  it('', () => {})
+
+  // is the price right?
+  it('', () => {})
+
+  // is redeemable? compared to my current point
+  it('', () => {})
+
+  it('', () => {})
+  it('', () => {})
+})
